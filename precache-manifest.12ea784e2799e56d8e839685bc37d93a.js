adea98c7e655040fe3e78b9b119ecc4383b78704
strannik <strannik.mas@gmail.com>
2021-06-17 10:44:45 +0300

    add phone to lead

---
+++ precache-manifest.12ea784e2799e56d8e839685bc37d93a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efba64675b934f40cd9d784e018a8ad6",
+    "url": "/.htaccess"
+  },
+  {
+    "revision": "5f3a6194bc52ba577744",
+    "url": "/css/app.fb5c88c1.css"
+  },
+  {
+    "revision": "8571cb81d1390bf96bda",
+    "url": "/css/chunk-vendors.dcf93e26.css"
+  },
+  {
+    "revision": "5b55309310a49c867ce4a6b8af7d0d8c",
+    "url": "/img/bg-header-mobile.jpg"
+  },
+  {
+    "revision": "e13034c23c24c742bb30c21964d67e3e",
+    "url": "/img/en.svg"
+  },
+  {
+    "revision": "68b4d8305c4f90577e6357bb0c247d29",
+    "url": "/img/en_br.svg"
+  },
+  {
+    "revision": "71dbb46044e8f441aad32fb9a91f8436",
+    "url": "/img/isr.svg"
+  },
+  {
+    "revision": "f73a242fad33037d8a2e4e8f04545423",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f3a6194bc52ba577744",
+    "url": "/js/app.098e8df1.js"
+  },
+  {
+    "revision": "8571cb81d1390bf96bda",
+    "url": "/js/chunk-vendors.8914abb8.js"
+  },
+  {
+    "revision": "8eff00fb23e5046e8587fb699e79e093",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "df75f9bbe4515147d16670092806b858",
+    "url": "/video/contact.mp4"
+  }
+]);
