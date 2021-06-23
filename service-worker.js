0dbc44d511885070136cdb4c04e4410fcbb7cf5b
strannik <strannik.mas@gmail.com>
2021-06-23 16:32:43 +0300

    fix change domain

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.89bf70cc22eac17aaf184b88202e7710.js"
+  "/precache-manifest.12ea784e2799e56d8e839685bc37d93a.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "client"});
